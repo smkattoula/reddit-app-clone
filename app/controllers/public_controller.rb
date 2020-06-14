159e7fd2c1b6c1f740bfdce5fa029a06c1551fcf
Shaker Kattoula <smkattoula@gmail.com>
2020-06-14 13:38:52 -0700

    create new post for community

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,5 +2,6 @@ class PublicController < ApplicationController
 
     def index
         @communities = Community.all.limit(5)
+        @posts = Post.order(id: :desc).limit(20)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :accounts
-  resources :communities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :communities do
+    resources :posts
+  end
+  
   root to: "public#index"
 end
